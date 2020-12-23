b5fea479a18cab338e5f5ac1a19e484c7f64e0f2
Arpan Sahoo <arpan.sahoo@ad.infosys.com>
2020-12-23 18:03:59 +0530

    Logout Implemented

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { useSelector, useDispatch } from "react-redux";
+import { logOut } from '../actions/userActions'
 const Header = () => {
-    return (
-<nav class="navbar navbar-expand-md fixed-top navbar-light bg-light">
-    <Link class="navbar-brand" href="#">EShop</Link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        
+
+    const dispatch = useDispatch();
+    const handleLogout=()=>{
+        dispatch(logOut());
+    }
+    const auth = useSelector((state) => state.user);
+
+    const guestLinks=(
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
                 <Link class="nav-link" href="#">Login</Link>
@@ -18,6 +19,34 @@ const Header = () => {
                 <Link class="nav-link" href="#">Register</Link>
             </li>
         </ul>
+    )
+
+    const authLinks=(
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <Link class="nav-link" href="#">Create Product</Link>
+            </li>
+            <li class="nav-item">
+                <Link class="nav-link" href="#">{auth?.userInfo?.name}</Link>
+            </li>
+            <li class="nav-item">
+                <Link class="nav-link" href="#">Shop</Link>
+            </li>
+            <li class="nav-item">
+                <Link onClick={handleLogout} class="nav-link" href="#">Logout</Link>
+            </li>
+        </ul>
+    )
+
+return (
+<nav class="navbar navbar-expand-md fixed-top navbar-light bg-light">
+    <Link class="navbar-brand" href="#">EShop</Link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+     {auth?.userInfo?authLinks:guestLinks}   
+        
     </div>
 </nav>    )
 }
